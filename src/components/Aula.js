5d88d06d9d1e6a65e6f42807ee1ea3e89c3fcaef
rayaneanjos <raydosanjos98@gmail.com>
2023-03-22 21:55:36 -0300

    Criando um hook próprio

--- src/components/Aula.js
+++ src/components/Aula.js
@@ -2,22 +2,19 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import data from '../Data'
 
-export default function Aula(props){
-    const {id} = useParams()
+function useAula(){
+    const id = useParams().id
     
+    return data.filter(aula =>aula.id == id )[0]
+}
+
+export default function Aula(props){
+    const aula = useAula()
+
     return(
         <div className="page">
-            {data.map(aulas=>(
-              aulas.id==id ? (<div>
-                    <ul className="aulas">
-                        <li>{}</li>
-                        <li>{aulas.title}</li>
-                        <li>{aulas.desc}</li>
-                    </ul>
-                </div>):(
-                    <div></div>
-                ))
-                )}
+            <h1>{aula.title}</h1>
+            <h3>{aula.desc}</h3>
         </div>
     )
 }
